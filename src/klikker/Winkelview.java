de0e4e3a475b1a26ff0179ef5bdd31eed6dfb17e
Damian <Damian.berns@student.aventus.nl>
2018-06-07 09:45:38 +0200

    Winkelview

--- src/klikker/Winkelview.java
+++ src/klikker/Winkelview.java
@@ -1,5 +1,26 @@
 package klikker;
 
-public class Winkelview {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Winkelview extends JFrame{
+
+	JLabel UpgradeTitel;
+	
+	private JPanel KoopKnop;
+
+	public void createPanel2(){
+		KoopKnop = new JPanel();
+		KoopKnop.setLayout( new FlowLayout() );
+
+	JLabel UpgradeTitel = new JLabel("Upgrade naam");
+		
+		KoopKnop.add( new JButton( "Koop" ) );
+	}
+
 
 }
